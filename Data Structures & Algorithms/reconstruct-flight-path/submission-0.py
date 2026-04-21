df91a73691214ee78c76e9807db71a7c1c9be72c
shine7on <103234196+shine7on@users.noreply.github.com>
2026-04-21 14:55:13 -0500

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # YouTube Answer
+        # backtracking
+        adj = { src : [] for src, dst in tickets }
+
+        tickets.sort()
+        for src, dst in tickets:
+            adj[src].append(dst)
+        
+        res = ['JFK']
+
+        def dfs(src):
+            if len(res) == len(tickets) + 1:
+                return True
+
+            # no path going out from src
+            if src not in adj:
+                return False
+
+            temp = list(adj[src])
+            for i, v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+
+                if dfs(v): return True
+
+                adj[src].insert(i,v)
+                res.pop()
+            return False
+
+        dfs('JFK')
+        return res
+
+
